f44b69ed0fce89517cd7ada1a7b845c1486014ec
admin <admin@DESKTOP-QU5NRFA>
2023-07-13 15:40:01 +0900

    first commit

---
+++ HelloWorld/src/co/yedam/Mainexe.java
@@ -0,0 +1,11 @@
+package co.yedam;
+
+public class Mainexe {
+	public static void main(String[] args) {
+		System.out.println("hello, gihwan");
+		
+		PrintExe printExe = new PrintExe(); 
+		printExe.showInfo();
+	}
+	
+	}
