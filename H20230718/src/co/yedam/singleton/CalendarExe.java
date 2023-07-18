9c9ff1919bd312c249f036cb2d1a1e77b8ee1cdd
admin <admin@DESKTOP-QU5NRFA>
2023-07-18 17:47:25 +0900

    commit!

--- H20230718/src/co/yedam/singleton/CalendarExe.java
+++ H20230718/src/co/yedam/singleton/CalendarExe.java
@@ -1,10 +1,30 @@
 package co.yedam.singleton;
 
 import java.util.Calendar;
+import java.util.Scanner;
 
 public class CalendarExe {
 	public static void main(String[] args) {
-		showCal(2023,7);
+//		showCal(2023,7);
+		Scanner scn = new Scanner(System.in);
+		System.out.println("input>>> ");
+		String str;
+		
+		String no = scn.next();
+		String name = scn.next();
+		String score = scn.next();
+		System.out.println("번호: " + no + " 이름: " + name + " 점수:" + score);
+		
+		//str = scn.nextLine();
+		//System.out.println(str);
+//		while (true) {
+//			if (scn.hasNext()) {
+//				str = scn.next();
+//				System.out.println(str);
+//				continue;
+//			}
+//			break;
+//		}
 	}
 
 	public static void showCal(int year, int month) {
